b47fe39ebf7a3b0c46f9760f00552d94717e2ac6
Danish Fahim <danisharyanfahim@gmail.com>
2025-01-10 14:47:48 -0500

    added header logo and changed fonts

--- app/(pages)/movies/page.tsx
+++ app/(pages)/movies/page.tsx
@@ -13,6 +13,12 @@ const Index = async ({
   const numberOfMovies: number = await getNumberOfMovies(searchParams);
   return (
     <div className="movie-page">
+      <header>
+        <h1 className="title-container">
+          Movies
+          <span className="colored">App</span>
+        </h1>
+      </header>
       <SearchBar />
       <InfiniteScroll
         searchParams={searchParams}
